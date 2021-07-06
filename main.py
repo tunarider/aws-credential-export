bf42a33f7166cd72ef8ed288c65ac6b1ee234878
tunarider <tunarider@tunaground.net>
2021-07-06 19:42:41 +0900

    config 대응

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import os
 import configparser
 import argparse
@@ -9,8 +10,12 @@ if __name__ == "__main__":
     parser.add_argument("--config", type=str, default="~/.aws")
     args = parser.parse_args()
 
+    credentials = configparser.ConfigParser()
+    credentials.read(os.path.expanduser(os.path.join(args.config, "credentials")))
     config = configparser.ConfigParser()
-    config.read(os.path.expanduser(os.path.join(args.config, "credentials")))
+    config.read(os.path.expanduser(os.path.join(args.config, "config")))
 
-    for key in config[args.profile]:
-        print("export {}={}".format(key.upper(), config[args.profile][key]))
+    for key in credentials[args.profile]:
+        print("export {}={}".format(key.upper(), credentials[args.profile][key]))
+    for key in config["profile {}".format(args.profile)]:
+        print("export AWS_DEFAULT_{}={}".format(key.upper(), config["profile {}".format(args.profile)][key]))
